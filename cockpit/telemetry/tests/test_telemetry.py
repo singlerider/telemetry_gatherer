5caa5df36fff0f8a5ce3af6f87688355af0326b6
shane <contact@shane.gg>
2020-10-22 22:20:36 -0700

    Add initial testing framework

---
+++ cockpit/telemetry/tests/test_telemetry.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from cockpit.telemetry.models import Machine
+
+class TemperatureTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_machine_has_temperature(self):
+        pass
